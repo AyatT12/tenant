ccaebf129273eae0a2dba69b0e316f7f244e2625
AyatT12 <105029664+AyatT12@users.noreply.github.com>
2025-10-13 17:34:32 +0300

    Add files via upload

--- js/tenant.js
+++ js/tenant.js
@@ -1,22 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.body.addEventListener("keydown", function (event) {
-        if (event.key === "Enter") {
-            document.getElementById("tenant-form").onsubmit = function (event) {
-                event.preventDefault();
-                console.log("Submitted Data:");
-                console.log("Search Query:", document.getElementById("search").value);
-                console.log("Selected Option:", document.querySelector('input[name="Options"]:checked').value);
-               
-            };
-        }
-    });
-
-
-});
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 document.addEventListener("DOMContentLoaded", function() {
-  var tableRows = document.getElementById("myTable").getElementsByTagName("td");
+  var tableRows = document.getElementById("Tenants-Table").getElementsByTagName("td");
   for (var i = 0; i < tableRows.length; i++) {
     tableRows[i].addEventListener("click", function() {
       window.location.href = "tenant-data.html"; 
@@ -25,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-var rows = document.getElementById("myTable").getElementsByTagName("tr");
+var rows = document.getElementById("Tenants-Table").getElementsByTagName("tr");
 for (var i = 0; i < rows.length; i++) {
   rows[i].onclick = function () {
     var cells = this.getElementsByTagName("td");
